5c01d71a33022611c3a03d9690bbbc01ef9f8846
Phan Quang <quangphanvan@vccorp.vn>
2026-06-25 13:58:23 +0700

    Configure library product as dynamic to prevent crashes

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "IGListKit_AdmicroAdsSDK",
+            type: .dynamic,
             targets: ["IGListKit_AdmicroAdsSDK"]
         ),
     ],
